5bace47b4c101dc6b00b0327fc10735af9b835fb
Sean Champ <gimbal+github@metacommunity.info>
2014-12-11 12:23:44 -0800

    export symbols

--- src/main/cltl/dnet-uri-package.lisp
+++ src/main/cltl/dnet-uri-package.lisp
@@ -5,4 +5,12 @@
 (defpackage #:info.metacommunity.cltl.dnet.uri
   (:nicknames #:dnet.uri)
   (:use #:info.metacommunity.cltl.utils #:cl)
-  )
+  (:export
+   #:uri-table
+   #:uri-hash-table
+   #:make-uri-table
+   #:uri-get
+   #:uri-put
+   #:uri-delete
+   #:uri-equal
+   ))
